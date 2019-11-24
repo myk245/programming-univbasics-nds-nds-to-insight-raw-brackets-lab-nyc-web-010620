69ee9349cef7de1a0bc701d25a34be00f7412a68
Cindy Kei <myk245@nyu.edu>
2019-11-24 03:13:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -23,7 +23,7 @@ def directors_totals(nds)
   end
   row_index += 1
 end
-
+return directors_totals(directors_database)
 
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
