634212bfed52ac1dd9aaf98ef40b8f775b0c7c96
Cindy Kei <myk245@nyu.edu>
2019-11-24 03:03:45 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -16,8 +16,13 @@ def directors_totals(nds)
       while inner_index < inner_len do
         directors_totals += directors_database[row_index][column_index][inner_index]
 [:worldwide_gross]
-
+      inner_index += 1
     end
+    column_index += 1
+  end
+  row_index += 1
+end 
+
 
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
