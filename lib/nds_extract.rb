bf1581c06a4532eb682abe11fb1893e8e5429199
Cindy Kei <myk245@nyu.edu>
2019-11-24 03:27:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,18 +9,18 @@ pp directors_database
 def directors_totals(nds)
   result = {}
   row_index = 0
-  while row_index < nds.length do
+  while row_index < directors_database.length do
     column_index = 0
     total = 0
-    while column_index < nds[row_index][:movies].length do
-      total += nds[row_index][:movies][column_index][:worldwide_gross]
+    while column_index < directors_database[row_index][:movies].length do
+      total += directors_database[row_index][:movies][column_index][:worldwide_gross]
       column_index += 1
   end
-  result[nds[row_index]][:name]
+  result[directors_database[row_index]][:name]
   row_index += 1
 end
 return results
-end 
+end
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
